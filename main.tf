6d28fbd34faa14687f17890fcea3884ecc742895
Billy Jessop <williamjessop@white@ubuntu>
2023-01-31 15:04:53 +0000

    Adding ubuntu server

--- main.tf
+++ main.tf
@@ -15,3 +15,18 @@ resource "aws_instance" "amazonlinux" {
   ]
 }
 
+resource "aws_instance" "ubuntu_server" {
+  ami = local.ami_ubuntu_server
+  instance_type = local.ami_ubuntu_server_instance_type
+  vpc_security_group_ids = [local.my_security_groupid]
+  key_name = "labkey"
+
+    tags = {
+
+    Name = "UBuntu Server",
+
+  }
+  depends_on = [
+    aws_key_pair.my_new_key
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -6,6 +6,13 @@ locals {
   my_security_groupid = aws_security_group.demo-sg.id
 }
 
+locals {
+  ami_ubuntu_server = "ami-0ab0629dba5ae551d"
+  ami_ubuntu_server_instance_type = "t2.micro"
+  ami_ubuntu_server_vpc  = "vpc-ad8920c6"
+  ami_ubuntu_server_security_groupid = "ami_ubuntu_server_aws_security_group.id"
+}
+
 output "public_ip" {
 
   value       = aws_instance.amazonlinux.public_ip
@@ -30,4 +37,9 @@ output "key_pair_id" {
 
   value = aws_key_pair.labkey.id
   description = "New key pair ID"
+}
+
+output "ubuntu_server_public_ip"{
+  value = aws_instance.ubuntu_server.public_ip
+  description = "Public IP Address of ubuntu EC2 instance"
 }
