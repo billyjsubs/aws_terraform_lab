3980917bfb1fc3388bd6377ef87fc77d1591c4fa
Billy JEssop <Billy JEssop>
2023-02-01 19:28:44 +0000

    Bash

---
+++ amazon_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Documents/code/git/aws_terraform_lab
+ssh -i "/home/williamjessop/Documents/code/git/Keys/freelab.pem" ec2-user@ec2-18-224-45-252.us-east-2.compute.amazonaws.com 
---
+++ ubuntu_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Documents/code/git/aws_terraform_lab
+ssh -i "/home/williamjessop/Documents/code/git/Keys/freelab.pem" ubuntu@ec2-18-216-229-16.us-east-2.compute.amazonaws.com
